7f54d1c9fcfa7b1722bc98c052425065a08c24bd
GazApp <juangaz@hotmail.com>
2022-01-09 15:40:29 -0600

    commit for math

--- 02_Math.php
+++ 02_Math.php
@@ -1,1 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
 <?php
+$math = "<h3> CLASS MATH </h3>";
+echo $math;
+echo "<br>";
+
+echo 56 + 45;
+echo "<br>";
+echo 56 - 45;
+echo "<br>";
+echo 56 * 45;
+echo "<br>";
+echo 56 / 45;
+echo "<br>";
+echo "<br>";
+
+
+
+echo 45 + 34 * 45 /421 - 45;
+echo "<br>";
+echo 5 + 5 / 10;
+
+echo "<br>";
+
+
+$number1 = 12;
+$number2 = 24;
+
+echo $number1 * $number2;
+echo "<br>";
+echo $number1 / $number2;
+echo "<br>";
+echo $number1 - $number2;
+echo "<br>";
+echo $number1 + $number2;
+
+?>
+
+</body>
+</html>
